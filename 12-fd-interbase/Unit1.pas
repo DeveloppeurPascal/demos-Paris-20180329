55692b3ee51d135790deeb6e4da59da88494cf70
Développeur Pascal <patrick.premartin@developpeur-pascal.fr>
2019-03-22 21:51:36 +0100

    Add files via upload

---
+++ 12-fd-interbase/Unit1.pas
@@ -0,0 +1,37 @@
+unit Unit1;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, FireDAC.Stan.Intf, FireDAC.Stan.Option, FireDAC.Stan.Error, FireDAC.UI.Intf, FireDAC.Phys.Intf, FireDAC.Stan.Def,
+  FireDAC.Stan.Pool, FireDAC.Stan.Async, FireDAC.Phys, FireDAC.Phys.IB, FireDAC.Phys.IBDef, FireDAC.VCLUI.Wait, FireDAC.Stan.Param, FireDAC.DatS,
+  FireDAC.DApt.Intf, FireDAC.DApt, Data.DB, FireDAC.Comp.DataSet, FireDAC.Comp.Client, Vcl.ExtCtrls, Vcl.DBCtrls, Vcl.Grids, Vcl.DBGrids, Vcl.StdCtrls,
+  RzLstBox, RzDBList, IWVCLBaseControl, IWBaseControl, IWBaseHTMLControl, IWControl, IWCompListbox, IWDBStdCtrls;
+
+type
+  TForm1 = class(TForm)
+    DBGrid1: TDBGrid;
+    DBNavigator1: TDBNavigator;
+    DataSource1: TDataSource;
+    EmployeeConnection: TFDConnection;
+    EmployeeTable: TFDQuery;
+    Employee_projectTable: TFDQuery;
+    ProjectTable: TFDQuery;
+    DataSource2: TDataSource;
+    DataSource3: TDataSource;
+    DBGrid2: TDBGrid;
+  private
+    { Déclarations privées }
+  public
+    { Déclarations publiques }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+end.
